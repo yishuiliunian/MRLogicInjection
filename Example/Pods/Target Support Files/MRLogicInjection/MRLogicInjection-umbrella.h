ed2fe65e08a824c24e5e8bbd8f0612ef364a9ed8
yishuiliunian <yishuiliunian@gmail.com>
2016-08-08 17:24:53 +0800

    x

--- Example/Pods/Target Support Files/MRLogicInjection/MRLogicInjection-umbrella.h
+++ Example/Pods/Target Support Files/MRLogicInjection/MRLogicInjection-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "MRExtendClass.h"
+#import "MRLogicInjection.h"
 
 FOUNDATION_EXPORT double MRLogicInjectionVersionNumber;
 FOUNDATION_EXPORT const unsigned char MRLogicInjectionVersionString[];
